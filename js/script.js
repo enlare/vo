3c9138cfbe491d00c10d77b135813554502efdd3
enlare <linn.coffeein@gmail.com>
2014-06-04 21:58:40 +0400

    map

--- js/script.js
+++ js/script.js
@@ -2,4 +2,25 @@ $(document).ready(function() {
     $('#myCarousel').carousel({
         interval: 4000
     });
+    
+    ymaps.ready(init);
+    function init() {
+        var map = new ymaps.Map("map", {
+            center: [55.648245,37.265238,17],
+            zoom: 15
+        });
+
+        metka = new ymaps.GeoObject({
+            geometry: {
+                // Тип геометрии - точка
+                type: "Point",
+                // Координаты точки.
+                coordinates: [55.648245,37.265238,17]
+            }
+        });
+
+        // Добавляем на карту
+        map.geoObjects.add(metka);
+    }
+
 });
