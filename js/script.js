f023413c85084f3197f08938bbdc71b22351dd37
enlare <linn.coffeein@gmail.com>
2014-06-06 21:30:42 +0400

    rooms page

--- js/script.js
+++ js/script.js
@@ -2,11 +2,11 @@ $(document).ready(function() {
     $('#myCarousel').carousel({
         interval: 4000
     });
-    
+
     ymaps.ready(init);
     function init() {
         var map = new ymaps.Map("map", {
-            center: [55.648245,37.265238,17],
+            center: [55.648245, 37.265238, 17],
             zoom: 15
         });
 
@@ -15,25 +15,24 @@ $(document).ready(function() {
                 // Тип геометрии - точка
                 type: "Point",
                 // Координаты точки.
-                coordinates: [55.648245,37.265238,17]
+                coordinates: [55.648245, 37.265238, 17]
             }
         });
 
         // Добавляем на карту
         map.geoObjects.add(metka);
     }
-    
-     $(document).on('click touchstart', '.up', function() {
+
+    $(document).on('click touchstart', '.up', function() {
         $('body').scrollTop(0);
     });
-    
-   
 
 
- $('#rooms-carousel-0').carousel({
+    $('#rooms-carousel-0').carousel({
         interval: 4000
     });
-// handles the carousel thumbnails
+
+// когда ткнул пользователь 
     $(document).on('click', '.thumb-item-link', function() {
         var sender = $(this);
         var id = parseInt(sender.data('number'));
@@ -42,15 +41,11 @@ $(document).ready(function() {
         sender.find('.thumb-img').addClass('selected-thumb');
     });
 
-// when the carousel slides, auto update
+// когда автоматически крутится
     $('#rooms-carousel-0').on('slid.bs.carousel', function() {
         var id = $('.item.active').data('slideNumber');
         id = parseInt(id);
         $('.thumb-img').removeClass('selected-thumb');
         $('#carousel-selector-' + id).find('.thumb-img').addClass('selected-thumb');
     });
-    
-// $('#slider-thumbs').carousel({
-////        interval: 1000
-//    });
 });
